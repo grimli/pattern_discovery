ade0e001f357b1d892d49ef9c3a8b5da56a356fb
grimli <grimli_clex@yahoo.it>
2015-02-20 12:03:22 +0100

    cleaned code

--- patternDiscovery.py
+++ patternDiscovery.py
@@ -2,7 +2,7 @@
 from pprint import pprint
 
 def FrequentElements( elements = [], transactions = [], minSup = 0.5, debug = False ):
-    """ This function get as input the list of the element to be tested, the list of transactions and the values of minimun support e minimum Confidence.
+    """ This function get as input the list of the element to be tested, the list of transactions and the values of minimun support.
         The function returns a dictionary that has as keys the frequent elements and values a list with the count and the support
     """ 
     frequencies = {};
@@ -10,6 +10,7 @@ def FrequentElements( elements = [], transactions = [], minSup = 0.5, debug = Fa
     label = '';
     numTransactions = len(transactions);
 
+    # calculate frequencies
     for element in elements:
         if isinstance( element, str ):
             for transaction in transactions:
@@ -18,31 +19,24 @@ def FrequentElements( elements = [], transactions = [], minSup = 0.5, debug = Fa
                 except:
                     allFrequencies[ element ] = transaction.count( element );
         else:
-            #print( 'flag' );
             label = '';
             for a in element:
                 label += ','+a;
-                #label += a;
 
             label=label.lstrip(',');
-            #print( label );
 
             for transaction in transactions:
                 flag = 0;
-                #print( "transaction = "+str(transaction) );
-                #print( "flag = "+str(flag) );
                 for field in element:
                     if ( transaction.count( field ) == 0 ):
                         flag -= 1;
                         break;
 
                 if flag >= 0:
-                    #print( "transaction = "+str(transaction) );
                     try:
                         allFrequencies[ label ] += 1;
                     except:
                         allFrequencies[ label ] = 1;
-                    #print( allFrequencies );
 
     # purging dictionary
     for element in elements:
@@ -68,7 +62,6 @@ def generateNextItemsetList( previousList = [], transactions = [] , debug = Fals
     """
     if debug: print( previousList );
     k = len(previousList);
-    #print( k );
     elements = [];
     # if the list is empty i generate all the possible 1-itemset
     if ( k == 0 ):
@@ -80,6 +73,7 @@ def generateNextItemsetList( previousList = [], transactions = [] , debug = Fals
                except:
                    elements.append( [element] );
     # if the itemset is not empty join as for apriori
+    # yes else is part of the for cicle
     else:
         count = 0;
         for i in range( 0, k ):
@@ -94,7 +88,6 @@ def generateNextItemsetList( previousList = [], transactions = [] , debug = Fals
                         if debug : print( previousList[i][0] );
                         for l in range ( 0, len(previousList[i])-1 ):
                             elements[count].append( previousList[i][l] );
-                            #print( "pippo\n" );
                         elements[count].append( previousList[i][len(previousList[i])-1] );
                         elements[count].append( previousList[j][len(previousList[j])-1] );
                         elements[count] = sorted( elements[count] );
@@ -104,7 +97,13 @@ def generateNextItemsetList( previousList = [], transactions = [] , debug = Fals
     if debug : print( sorted(elements) );
     return sorted(elements);
 
-# parto da file fisso elenco.txt con righe come
+# to mantain the exemple simple i use a csv file like
+# farina,uova,latte,banana,patata,biscotto,cioccolatino
+# farina,uova,latte,biscotto,tavolino,cioccolatino
+# farina,uova,latte,patata,carne,cavolfiore
+# mela,banana,pera,biscotto
+# pannolini,fazzoletto,patata
+
 debug = False;
 file = 'elenco.txt';
 sep= ',';
@@ -119,7 +118,6 @@ a.close()
 # won't be added to 2-itemsets (i.e. Beer,Diaper and Diaper,Beed)
 itemList = [sorted(item.rstrip('\n').split(sep)) for item in itemList]
 elementList = generateNextItemsetList( [], itemList );
-#print ( elementList );
 
 frequencies = FrequentElements( elementList, itemList , minSup );
 print( frequencies );
